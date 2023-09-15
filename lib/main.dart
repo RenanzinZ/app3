1c07866e47924b052e949706b47d11436041db30
RenanzinZ <renancoutin@gmail.com>
2023-09-15 16:49:29 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:app03/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
